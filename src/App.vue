5763a3a4b5d4df839dfeb82e9eeddefb68231a1c
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-02 14:38:08 -0300

    correções

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 	#app
-		img(src="./assets/logo.png")
 		router-view
 
 </template>
@@ -13,7 +12,32 @@
 
 
 <style lang="stylus">
-	#app
-		font-family verdana
+	@import url('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300')
+
+	*
+		margin 0
+		box-sizing border-box 
+		font-family 'Open Sans Condensed', sans-serif
+
+	body 
+		background #000 url(./assets/bg.jpg) center center no-repeat 
+		background-attachment fixed
+		background-size cover
+
+	a
+		text-decoration none
+
+	.main
+		width 90%
+		margin auto	
+
+	.flex
+		display flex
+		justify-content space-between
+		align-items center
+		
+	@media screen and (min-width: 992px)
+		.main
+			width 80%
 
 </style>
