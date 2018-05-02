9fee5282dbf5a783673bdc47b5ec81ccc683fa3c
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-02 14:40:09 -0300

    layout

---
+++ src/components/layout/AcoesPromovidas.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+    #acoespromovidas
+        h1 Ações Promovidas pela Ong
+        
+        section(style="display:flex;flex-wrap:wrap;justify-content:space-around")
+            Circulo(title="Projeto adote um amigo")
+            Circulo(title="Evento de caridade")
+            Circulo(title="Coleta de rações")
+            Circulo(title="Reunião com parceiros")
+            Circulo(title="Novas parcerias")
+            Circulo(title="Evento mensal de arrec...")
+
+</template>
+
+<script>
+    import Circulo from '../share/Circulo.vue'
+
+	export default {
+		name: 'AcoesPromovidas',
+        components:{
+            Circulo
+        }
+	}
+</script>
+
+
+<style lang="stylus">
+    #acoespromovidas
+        padding 5%
+        background-color rgba(255,255,255,1)
+        background url(../../assets/computer.png) center center no-repeat 
+        background-attachment fixed
+        background-size cover
+
+        & > h1
+            font-size 2.5em
+            text-align center
+            color #fff
+            text-transform uppercase
+            text-shadow 1px 1px 1px #000
+            margin-bottom 50px
+
+        & section
+            width 90%
+            padding-top 20px
+            margin auto
+
+    @media screen and (min-width: 768px)
+        #acoespromovidas section
+            width 80%
+
+</style>
