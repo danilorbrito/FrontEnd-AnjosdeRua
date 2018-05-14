46256557aba9ada9a412f92d7bd1cd13fa2ffae3
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-13 23:24:30 -0300

    remoção de desnecessários

--- src/store/modules/usuario/getters.js
+++
@@ -1,5 +0,0 @@
-export default { 
-	getUser: state => {return state.user},
-	getToken: state => {return state.token},
-	getApi: state => {return state.api}
-}
--- src/store/modules/usuario/state.js
+++
@@ -1,5 +0,0 @@
-export default { 
-	user:{},
-	token:{},
-	api:'http://localhost:3030/'
-}
