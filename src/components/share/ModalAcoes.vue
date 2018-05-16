ae26b7bf18df0f8358e847ea8d2e41c1b2f4e1d9
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-16 11:19:14 -0300

    construção da modalAções

---
+++ src/components/share/ModalAcoes.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    div
+        div(:class="{'modal-frame':true, 'state-appear':status}")
+            .modal
+                .modal-inset
+                    .button.close(@click="status=false") x
+                    .modal-body
+                        slot(name="body")    
+        div(:class="{'modal-overlay':true, 'state-show':status}")
+            
+</template>
+
+<script>
+    import { EventBus } from '../../helpers/eventBus.js'
+
+	export default {
+        //props:["status"],
+        data()
+        {
+            return{
+                status:false
+            }
+        },
+        name: 'Modal',
+        mounted(){
+            EventBus.$on('showAction', acao => {
+                this.status = true
+                console.log( JSON.stringify(acao) )
+            })
+        }
+	}
+</script>
+
+<style lang="stylus" >
+	@import '../../styl/Modal.styl'
+</style>
