02c48038daa26b4d7c86eb340c1c529e0e15a018
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-23 10:56:50 -0300

    refatorando store adoções

--- src/store/modules/adocoes/state.js
+++ src/store/modules/adocoes/state.js
@@ -1,28 +1,3 @@
 export default { 
-	adocoes:[
-		{
-			id:1,
-			associado:{
-				id:1, 
-				nome:'batman', 
-				sexo:"f", 
-				email:"email@email.com", 
-				telefones:[{id:1,numero:'34353434'},{id:2,numero:'6754'}],
-				pass:"123",
-				endereco:{rua:'rua1',numero:'100',bairro:'bairro',cidade:'matão',estado:'sp',cep:'200'}
-			},
-			animal:{
-				id:1, 
-				nome:'duque', 
-				raca:"raça 1", 
-				descricao:"descrição 1", 
-				cor:"preto", 
-				sexo:"f", 
-				idade:7,
-				adotado:false
-			},
-			datahora:"10/04/2018 12:00",
-			novasmensagens:3
-		}
-	]
+	adocoes:[]
 }
